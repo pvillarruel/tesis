61b3de9982ba6edc44b843d8a3b4bd4278b935cd
pvillarruel <blablo@hotmail.es>
2015-10-18 19:30:01 -0500

    proyecto tesis yankiy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tesis::Application.config.secret_key_base = 'ea832106ce6999ee1ad03e7c79529cd1efd0977134f3123358a78c622fe515692736ae06ed50e701826eeaf6cd0dda143b6d072ff78a1a82fba79bbd141184d0'
